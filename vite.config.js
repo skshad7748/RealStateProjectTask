e2dba4ad8c48def2602664cf6681eb717cb23fec
Sk Shad <hasibul15-7748@diu.edu.bd>
2025-04-27 19:55:18 +0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || '/RealStateProjectTask',
 })
